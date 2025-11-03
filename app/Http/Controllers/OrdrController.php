d295ba83eb4263691b4e15bb08f9d1f1c0d536b9
Hadi Santoso <hadisnt15@gmail.com>
2025-11-03 09:45:29 +0800

    Hapus job pushordr

--- app/Http/Controllers/OrdrController.php
+++ app/Http/Controllers/OrdrController.php
@@ -297,8 +297,8 @@ class OrdrController extends Controller
 
     public function push()
     {
-        // Artisan::call('push:ordr');
-        \App\Jobs\PushOrdrJob::dispatch();
+        Artisan::call('push:ordr');
+        // \App\Jobs\PushOrdrJob::dispatch();
         return back()->with('success', 'Data Pesanan Berhasil Di-push ke SAP');
     }
 
@@ -448,7 +448,7 @@ class OrdrController extends Controller
 
         return redirect()->route('order')->with('success', 'Pengecekan pesanan berhasil diperbarui.');
     }
-    
+
     public function delete($id)
     {
         $order = OrdrLocal::findOrFail($id);
