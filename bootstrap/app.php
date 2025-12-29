a3c7a6f5e989e8a387077e6dc228a8a60baa2de7
Hadi Santoso <hadisnt15@gmail.com>
2025-12-29 15:07:48 +0800

    update autosync untuk report pembelian harian

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -262,6 +262,17 @@ return Application::configure(basePath: dirname(__DIR__))
         })->everyTwoHours()->when(function () {
             return now()->between(now()->setTime(8, 0), now()->setTime(20, 0));
         });
+
+        $schedule->call(function () {
+            Artisan::call('sync:reportBeliHarian');
+            SyncLog::create([
+                'name' => 'report.pembelian-harian',
+                'last_sync' => now(),
+                'desc' => 'Otomatis'
+            ]);
+        })->everyTwoHours()->when(function () {
+            return now()->between(now()->setTime(8, 0), now()->setTime(20, 0));
+        });
         // END REPORT DATA
     })
     ->create();
